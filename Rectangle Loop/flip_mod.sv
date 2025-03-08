c7f0d88de20bc6666ce010b011bff89f7d37f6a5
Annabelle Chen <150537808+annabchen@users.noreply.github.com>
2025-03-08 15:29:50 -0500

    Update flip_mod.sv

--- Rectangle Loop/flip_mod.sv
+++ Rectangle Loop/flip_mod.sv
@@ -1,12 +1,12 @@
 module flip
   #(parameter int ROWS = 4,
     parameter int COLS = 4) // arbitrary sizes
-  (input logic [ROWS][COLS] m_in,
+  (input logic [15:0] m_in,
    input logic [1:0] r1,
    input logic [1:0] r2,
    input logic [1:0] c1,
    input logic [1:0] c2,
-   output logic [ROWS][COLS] m_out);
+   output logic [15:0] m_out);
     
   logic clk;
   
@@ -16,22 +16,22 @@ module flip
   // creating a mask for every (r,c) combo
   logic [15:0] mask1;
   assign mask1 = mask1 + 1;
-  logic index1 = c1*ROWS + r1;
+  logic index1 = 15 - (c1*ROWS + r1);
   assign mask1 = (15'b1 << index1);
   
   logic [15:0] mask2;
   assign mask2 = mask2 + 1;
-  logic index2 = c2*ROWS + r1;
+  logic index2 = 15 - (c2*ROWS + r1);
   assign mask2 = (15'b1 << index2);
 
   logic [15:0] mask3;
   assign mask3 = mask3 + 1;
-  logic index3 = c1*ROWS + r2;
+  logic index3 = 15 - (c1*ROWS + r2);
   assign mask3 = (15'b1 << index3);
   
   logic [15:0] mask4;
   assign mask4 = mask4 + 1;
-  logic index4 = c2*ROWS + r2;
+  logic index4 = 15 - (c2*ROWS + r2);
   assign mask4 = (15'b1 << index4);
   
   logic [15:0] finalMask;
