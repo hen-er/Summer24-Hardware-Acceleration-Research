330edb05d15816252599bbc248efa93e87f712ee
Annabelle Chen <150537808+annabchen@users.noreply.github.com>
2025-03-08 15:46:16 -0500

    Update flip_mod.sv

--- Rectangle Loop/flip_mod.sv
+++ Rectangle Loop/flip_mod.sv
@@ -14,30 +14,33 @@ module flip
   
   // XOR the matrix with the index lengthened to match the form of the matrix as a string
   // creating a mask for every (r,c) combo
-  logic [15:0] mask1;
-  assign mask1 = mask1 + 1;
-  logic index1 = 15 - (c1*ROWS + r1);
-  assign mask1 = (15'b1 << index1);
+  always_comb
+    begin
+    logic [15:0] mask1;
+    assign mask1 = mask1 + 1;
+    logic index1 = 15 - (c1*ROWS + r1);
+    assign mask1 = (15'b1 << index1);
   
-  logic [15:0] mask2;
-  assign mask2 = mask2 + 1;
-  logic index2 = 15 - (c2*ROWS + r1);
-  assign mask2 = (15'b1 << index2);
+    logic [15:0] mask2;
+    assign mask2 = mask2 + 1;
+    logic index2 = 15 - (c2*ROWS + r1);
+    assign mask2 = (15'b1 << index2);
 
-  logic [15:0] mask3;
-  assign mask3 = mask3 + 1;
-  logic index3 = 15 - (c1*ROWS + r2);
-  assign mask3 = (15'b1 << index3);
+    logic [15:0] mask3;
+    assign mask3 = mask3 + 1;
+    logic index3 = 15 - (c1*ROWS + r2);
+    assign mask3 = (15'b1 << index3);
   
-  logic [15:0] mask4;
-  assign mask4 = mask4 + 1;
-  logic index4 = 15 - (c2*ROWS + r2);
-  assign mask4 = (15'b1 << index4);
+    logic [15:0] mask4;
+    assign mask4 = mask4 + 1;
+    logic index4 = 15 - (c2*ROWS + r2);  
+    assign mask4 = (15'b1 << index4); 
+    end
   
   logic [15:0] finalMask;
   assign finalMask = mask1 + mask2 + mask3 + mask4;
  
   // XOR the mask with the original matrix
-  m_out = m_in ^ finalMask;
+  assign m_out = m_in ^ finalMask;
     
 endmodule
