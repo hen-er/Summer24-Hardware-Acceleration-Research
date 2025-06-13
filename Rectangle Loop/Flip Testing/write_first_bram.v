caeb96f21124811ffc98c42777a926f5e898cf56
Hena Ershadi <136639931+hen-er@users.noreply.github.com>
2025-06-13 10:59:44 -0400

    Create write_first_bram.v

---
+++ Rectangle Loop/Flip Testing/write_first_bram.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module bram_port(
+    input clk, 
+    input write_en, 
+    input [7:0]address, 
+    input [7:0]data_in, 
+    output [7:0]data_out
+    );
+    
+    blk_mem_gen_1 bram(.clka(clk),.addra(address),.dina(data_in),.douta(data_out),.wea(write_en));
+    
+endmodule
