af00c84d949b6dd849cbe7245a8984694d63ece3
Hena Ershadi <136639931+hen-er@users.noreply.github.com>
2025-06-23 16:21:19 -0400

    Update write_first_bram.v
    added enable signal

--- Rectangle Loop/Flip Testing/write_first_bram.v
+++ Rectangle Loop/Flip Testing/write_first_bram.v
@@ -3,11 +3,12 @@
 module bram_port(
     input clk, 
     input write_en, 
+    input ena,
     input [7:0]address, 
     input [7:0]data_in, 
     output [7:0]data_out
     );
     
-    blk_mem_gen_1 bram(.clka(clk),.addra(address),.dina(data_in),.douta(data_out),.wea(write_en));
+    blk_mem_gen_1 bram(.clka(clk),.addra(address),.dina(data_in),.douta(data_out),.wea(write_en), .ena(ena));
     
 endmodule
